b2460b4caa62c56807650154fdb10dfb8331e2e5
Morpheu$ <64348796+JN3AZ@users.noreply.github.com>
2020-05-07 22:18:09 -0700

    added the function to do password generation, fixed syntax errors, added a clipboard to copy password & finalized html and js pages

--- script.js
+++ script.js
@@ -6,38 +6,95 @@ const lowercaseEl = document.getElementById('lowercase');
 const numbersEl = document.getElementById('numbers');
 const symbolsEl = document.getElementById('symbols');
 const generateEl = document.getElementById('generate');
-const clipboardEl = document.getElementById('clipboard');
+const clipboard = document.getElementById('clipboard');
+
 
 const randomFunc = {
-   lower: getRandomLower,
-   upper: getRandomUpper,
-   number: getRandomNumber,
-   symbol: getRandomSymbol,
-};
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol,
+}
 
+clipboard.addEventListener('click', () => {
+	const textarea = document.createElement('textarea');
+	const password = resultEl.innerText;
+	
+	if(!password) { return; }
+	
+	textarea.value = password;
+	document.body.appendChild(textarea);
+	textarea.select();
+	document.execCommand('copy');
+	textarea.remove();
+	alert('Password copied to clipboard');
+});
+
+
+// Generate event listen
 generateEl.addEventListener('click', () => {
-  const length = +lengthEl.value;
+  const length =  +lengthEl.value;
   const hasUpper = uppercaseEl.checked;
   const hasLower = lowercaseEl.checked;
   const hasNumber = numbersEl.checked;
   const hasSymbol = symbolsEl.checked;
 
-  console.log(hasLower, hasUpper, hasNumber, hasSymbol);
-});
+   resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
+}); 
 
+
+
+
+
+
+
+
+// Generate password function
+function generatePassword(lower, upper, number, symbol, length) {
+  let generatedPassword = '';
+
+  const typesCount = lower + upper + number + symbol;
+
+  // console.log('typesCount:', typesCount);
+
+  const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter
+  (
+    item => Object.values(item)[0]
+  );
+
+  // console.log('typesArr: ', typesArr);
+
+  if (typesCount === 0) {
+    return '';
+  }
+  // create a loop
+  for(let i = 0; i < length; i += typesCount) {
+    typesArr.forEach(type => {
+      const funcName = Object.keys(type)[0];
+
+      // console.log('funcName: ', FuncName);
+
+      generatedPassword += randomFunc[funcName]();
+    });
+  }
+
+  const finalPassword = generatedPassword.slice(0, length);
+
+  return finalPassword;
+}
 
 // Generator functions   -http://www.net-comber.com/charset.html
 
 function getRandomLower() {
-  return String.fromCharCode(Math.floor(Math.random() *26) + 97);
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
 
 function getRandomUpper() {
-  return String.fromCharCode(Math.floor(Math.random() *26) + 65);
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
 
 function getRandomNumber() {
-  return String.fromCharCode(Math.floor(Math.random() *10) + 48);
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
 
 function getRandomSymbol() {
@@ -45,21 +102,3 @@ function getRandomSymbol() {
   return symbols[Math.floor(Math.random() * symbols.length)];
 }
 
-
-
-
-
-// // Assignment Code
-// var generateBtn = document.querySelector("#generate");
-
-// // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-
-// }
-
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
