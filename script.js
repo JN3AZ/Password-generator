0c0870c16e03a07ceb6c7e7dd3281498e11e949e
Morpheu$ <64348796+JN3AZ@users.noreply.github.com>
2020-05-07 17:44:59 -0700

    added style.css page as well as created my DOM elements and Generator functions in js

--- script.js
+++ script.js
@@ -1,14 +1,53 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+//DOM elements
+const resultEl = document.getElementById('result');
+const lengthEl = document.getElementById('length');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numbersEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
+const generateEl = document.getElementById('generate');
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+const randomFunc = {
+   lower: getRandomLower,
+   upper: getRandomUpper,
+   number: getRandomNumber,
+   symbol: getRandomSymbol,
+};
 
-  passwordText.value = password;
+// Generator functions   -http://www.net-comber.com/charset.html
 
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() *26) + 97);
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() *26) + 65);
+}
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() *10) + 48);
+}
+
+function getRandomSymbol() {
+  const symbols = '!@#$%^&*(){}[]=<>/,.';
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+
+
+
+
+// // Assignment Code
+// var generateBtn = document.querySelector("#generate");
+
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
